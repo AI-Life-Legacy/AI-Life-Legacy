116e8ea0878e4936a2298484aab492bf0f5cdf72
ChoiGeon <qrwrqrwrer@naver.com>
2024-09-06 16:41:17 +0900

    fix: myprofile js 초기화

--- public/myprofile/check.js
+++ public/myprofile/check.js
@@ -11,22 +11,22 @@ document.addEventListener('DOMContentLoaded', async () => {
         if(logincheckResult.code != '200'){
             alert("로그인을 해주세요");
             window.location.href = "/login";
-        }else{
-            const existMyprofileResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/myprofile`,{
-                method: 'GET',
-                credentials: 'include',
-            });
-    
-            const isExistMPData = await existMyprofileResponse.json();
-            if(isExistMPData.code === "200"){ // 있는 경우
-                window.location.href = "/write/1";
-            }else{ // 없는 경우
-                document.getElementById('loading').style.display = 'none';
-                document.getElementById('content').style.display = 'flex';
-                // check.js 완료 이벤트 발생
-                const checkJsCompleteEvent = new Event('checkJsComplete');
-                document.dispatchEvent(checkJsCompleteEvent);
-            }
+        }
+
+        const existMyprofileResponse = await fetch(`https://asia-northeast3-life-legacy-dev.cloudfunctions.net/api/myprofile`,{
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        const isExistMPData = await existMyprofileResponse.json();
+        if(isExistMPData.code === "200"){ // 있는 경우
+            window.location.href = "/write/1";
+        }else{ // 없는 경우
+            document.getElementById('loading').style.display = 'none';
+            document.getElementById('content').style.display = 'flex';
+            // check.js 완료 이벤트 발생
+            const checkJsCompleteEvent = new Event('checkJsComplete');
+            document.dispatchEvent(checkJsCompleteEvent);
         }
     } catch (error) {
         console.error('Error fetching profile:', error);
